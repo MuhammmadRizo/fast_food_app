2e5e9ba6241407e1352a9de4b253979970babbc4
MuhammmadRizo <odilovadiyora52@gmail.com>
2025-08-19 12:06:53 +0500

    Firt App Project

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,12 @@
+import {View, Text, SafeAreaView} from 'react-native'
+import {Slot} from "expo-router";
+
+const _Layout = () => {
+    return (
+        <SafeAreaView>
+            <Text>Auth Layout</Text>
+            <Slot />
+        </SafeAreaView>
+    )
+}
+export default _Layout
